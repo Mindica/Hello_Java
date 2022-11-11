999a4bda994c2fb53ef410a8b36f2652b1ce3856
Novoselova Daria <dnovoselova@slcd.ru>
2022-11-11 17:00:18 +0300

    !!!

--- src/main/java/Mindica/Ultra/Programmer/MyFirstProg.java
+++ src/main/java/Mindica/Ultra/Programmer/MyFirstProg.java
@@ -2,7 +2,7 @@ package Mindica.Ultra.Programmer;
 
 public class MyFirstProg {
     public static void main(String[] args) {
-        System.out.println("Hello, world!");
+        System.out.println("Hello, world!!!");
     }
 
 }
