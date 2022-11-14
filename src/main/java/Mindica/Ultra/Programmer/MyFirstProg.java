e8f3ac8b89b5315001f022958a82ce4527799be6
Novoselova Daria <dnovoselova@slcd.ru>
2022-11-14 08:40:13 +0300

    Lesson 9

--- src/main/java/Mindica/Ultra/Programmer/MyFirstProg.java
+++ src/main/java/Mindica/Ultra/Programmer/MyFirstProg.java
@@ -3,6 +3,26 @@ package Mindica.Ultra.Programmer;
 public class MyFirstProg {
     public static void main(String[] args) {
         System.out.println("Hello, world!!!");
+
+        System.out.println(2 + 2);
+        System.out.println(2 * 2);
+        System.out.println(2 / 2);
+        System.out.println(2 - 2);
+
+        System.out.println(1 / 2);
+        System.out.println(1.0 / 2);
+        System.out.println(1 / 2.0);
+        System.out.println(2.0 / 2);
+
+        System.out.println("2" + "2");
+        System.out.println("2" + 2);
+        System.out.println(2 + "2");
+
+        System.out.println(2 + 2 * 2);
+        System.out.println((2 + 2) * 2);
+
+        System.out.println("2 + 2 = " + 2 + 2);
+        System.out.println("2 + 2 = " + (2 + 2));
     }
 
 }
